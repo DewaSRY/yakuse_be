1e156a03fc628490a136ad7fedb0d36a8a4cd9e3
DewaSRY <sdewa6645@gmail.com>
2024-07-31 15:31:35 +0800

    update:update uvicorn config

--- run.py
+++ run.py
@@ -9,4 +9,6 @@ import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    config = uvicorn.Config(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)), log_level="info")
+    server = uvicorn.Server(config)
+    server.run()

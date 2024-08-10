57293278b537dbf69d68b85bdad25c3a375b451e
DewaSRY <sdewa6645@gmail.com>
2024-08-10 21:26:58 +0800

    init:pytest as dev feature

--- run_test.py
+++ run_test.py
@@ -1,16 +1,23 @@
 import os
-
 import uvicorn
 
 from app.main import app
+from app.libs import sql_alchemy_lib
 
 
 def main():
+    # Drope all table first
+    sql_alchemy_lib.Base \
+        .metadata.drop_all(bind=sql_alchemy_lib.engine)
+    # Create new table thne
+    sql_alchemy_lib.Base \
+        .metadata.create_all(bind=sql_alchemy_lib.engine)
+
     # subprocess.call(["fastapi", "dev", "app/main.py"])
-    os.system("fastapi dev app/main.py")
+
+    os.system("pytest")
 
 
 if __name__ == "__main__":
-    print("hallo wrld")
     os.environ["IS_TESTING"] = "True"
     main()
---
+++ tests/user_services/test_user_create_access_token.py
@@ -0,0 +1,2 @@
+def test_first_test():
+    assert True is True

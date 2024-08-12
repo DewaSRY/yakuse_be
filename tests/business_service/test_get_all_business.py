2139e7bff496a62c53f8230aa926079c5953dfc1
DewaSRY <sdewa6645@gmail.com>
2024-08-12 16:02:31 +0800

    add:unitesting for get all business(1)

--- tests/business_service/test_get_all_business.py
+++ tests/business_service/test_get_all_business.py
@@ -34,3 +34,41 @@ def test_get_all_business_should_return_10(get_db, get_user_model):
         )
 
     assert len(business_services.get_all_business(get_db)) == 10
+
+
+def test_get_all_business_should_return_20(get_db, get_user_model):
+    list_business = []
+    for n in range(20):
+        create_business = business_dtos.BusinessCreateDto(
+            name=f"name {n} ",
+            description=f"description {n}",
+            location=f"some thing in earth {n}",
+            contact=f"some contact {n}",
+        )
+        list_business.append(create_business)
+
+    for business in list_business:
+        business_services.create_business(
+            db=get_db, business=business, user_id=get_user_model.id
+        )
+
+    assert len(business_services.get_all_business(get_db)) == 20
+
+
+def test_get_all_business_should_return_0(get_db, get_user_model):
+    # list_business = []
+    # for n in range(20):
+    #     create_business = business_dtos.BusinessCreateDto(
+    #         name=f"name {n} ",
+    #         description=f"description {n}",
+    #         location=f"some thing in earth {n}",
+    #         contact=f"some contact {n}",
+    #     )
+    #     list_business.append(create_business)
+
+    # for business in list_business:
+    #     business_services.create_business(
+    #         db=get_db, business=business, user_id=get_user_model.id
+    #     )
+
+    assert len(business_services.get_all_business(get_db)) == 0

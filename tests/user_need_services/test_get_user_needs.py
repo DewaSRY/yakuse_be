5a31f62b91279ae6abafbfafd152a22350e02ca5
DewaSRY <sdewa6645@gmail.com>
2024-08-13 08:40:01 +0800

    add:find way to mock python module

--- tests/user_need_services/test_get_user_needs.py
+++ tests/user_need_services/test_get_user_needs.py
@@ -2,15 +2,29 @@ import pytest
 
 from app.user import user_services, user_dtos
 from app.user_need import user_need_services, user_need_dtos
+from app.libs import sql_alchemy_lib
 
 
-def test_get_user_need_by_id(get_db):
-    user_model = user_services.create_user(
+@pytest.fixture()
+def get_user_model(get_db):
+    return user_services.create_user(
         get_db, user_dtos.UserCreateDto()).data
 
-    user_need_create = user_need_services.create_user_need_service(
-        db=get_db, user_need=user_need_dtos.UserNeedCreateDto(), user_id=user_model.id).data
+
+@pytest.fixture()
+def get_user_needs_model(get_db, get_user_model):
+    return user_need_services.create_user_need_service(
+        db=get_db, user_need=user_need_dtos.UserNeedCreateDto(), user_id=get_user_model.id).data
+
+
+def test_get_user_need_by_id(get_db, get_user_needs_model, monkeypatch):
+    def mock_get_db():
+        yield get_db
+
+    monkeypatch.setattr(sql_alchemy_lib, "get_db", mock_get_db)
+
     actual = user_need_services.get_user_need_by_id_service(
-        db=get_db, user_need_id=user_need_create.id
+        db=get_db, user_need_id=get_user_needs_model.id
     )
     assert actual is not None
+    assert actual.user_info is not None

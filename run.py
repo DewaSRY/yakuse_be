cea32c061dc23c5ba44bf93554e03e38b85b66f6
DewaSRY <sdewa6645@gmail.com>
2024-08-14 10:14:00 +0800

    fixed:remove .venv

--- run.py
+++ run.py
@@ -9,7 +9,6 @@ import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    print("hallo wrld")
     config = uvicorn.Config(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)), log_level="info")
     server = uvicorn.Server(config)
     server.run()

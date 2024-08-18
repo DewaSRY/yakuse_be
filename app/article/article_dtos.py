6204d0c644fcc4cbc049175b0a0ec8573a372d92
haikalbintang <mhaikalbintang.work@gmail.com>
2024-08-18 23:49:26 +0700

    add:add article_model and article_dtos

--- app/article/article_dtos.py
+++ app/article/article_dtos.py
@@ -1,16 +1,38 @@
 from datetime import datetime
-
+from typing import Optional
 from pydantic import BaseModel, Field
 
+
 class ArticleCreateDto(BaseModel):
-    title: str = Field(default="some articles from cool guy")
-    description: str = Field(default="this is description")
+    title: str = Field(default="article title from cool guy")
+    description: str = Field(default="some descriptions")
     image_url: str = Field(default="xxx")
+
+
+class ArticleCreateDto(BaseModel):
+    title: str = Field("article title from hot guy")
+    description: str = Field("some descriptionss")
+    image_url: str = Field("yyy")
+
+
+class ArticleAuthorInfoDto(BaseModel):
+    author_id: str
+    fullname: str
+    username: str
+    author_photo_url: Optional[str]
+
+
+class ArticleBusinessCategoryDto(BaseModel):
+    id: int = Field(1)
+    name: str
+
 
 class ArticleResponseDto(BaseModel):
     id: str
     title: str
     description: str
     image_url: str
+    author_info: ArticleAuthorInfoDto
+    category: ArticleBusinessCategoryDto
     created_at: datetime
     updated_at: datetime
--- app/article/article_model.py
+++ app/article/article_model.py
@@ -2,17 +2,48 @@ import uuid
 
 from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, Text, func
 from sqlalchemy.dialects.mysql import CHAR
+from sqlalchemy.orm import Mapped, relationship
 
 from app.libs import sql_alchemy_lib
+from app.article import article_dtos
 
 
 class Article(sql_alchemy_lib.Base):
     __tablename__ = "article"
+
     id = Column(CHAR(36), primary_key=True, default=lambda: str(uuid.uuid4()), unique=True, index=True)
-    title = Column(Text)
+    title = Column(String(255))
     description = Column(Text)
     image_url = Column(String(255))
+    
     created_at = Column(DateTime(timezone=True), server_default=func.now()) 
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    
     fk_business_category_id = Column(Integer, ForeignKey('business_category.id'))
     fk_author_id = Column(CHAR(36), ForeignKey('users.id'))
+
+    business_category: Mapped["BusinessCategory"] = relationship(viewonly=True)
+    author: Mapped["UserModel"] = relationship(viewonly=True)
+
+    @property
+    def author_info(self) -> dict[str, str]:
+        from app.user.user_model import UserModel
+        author_model: UserModel = self.author
+        return article_dtos.ArticleAuthorInfoDto(
+            author_id=author_model.id,
+            fullname=author_model.fullname,
+            username=author_model.username,
+            author_photo_url=author_model.photo_url
+        ).model_dump()
+    
+    @property
+    def category(self):
+        from app.business_category.business_category_model import BusinessCategory
+        business_category_model: BusinessCategory= self.business_category
+        return article_dtos.ArticleBusinessCategoryDto(
+            id=business_category_model.id,
+            name=business_category_model.name
+        ).model_dump()
+    
+    def __repr__(self):
+        return f"<Article: id({self.id}) title({self.title}) author_id({self.fk_author_id} category({self.fk_business_category_id}))>"

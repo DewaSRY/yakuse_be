525d152beb06d538f90f08e0229485491668fdf8
DewaSRY <sdewa6645@gmail.com>
2024-08-30 16:28:57 +0800

    fix:database

--- run.py
+++ run.py
@@ -12,14 +12,3 @@ if __name__ == "__main__":
     config = uvicorn.Config(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)), log_level="info")
     server = uvicorn.Server(config)
     server.run()
-# if __name__ == "__main__":
-#     TEXT = """
-#     hallo
-#     apakabar
-#      kalian semua
-#     """
-#     TEXT_LIST = re.split("\\s{4,}", TEXT)
-#
-#     print(len(TEXT_LIST))
-#     for s in TEXT_LIST:
-#         print(s)
